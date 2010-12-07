4eacb176b364be96a5b69f6b56b889bc35af539d
Adaptee <adaptee@gmail.com>
2010-12-08 05:38:10 +0800

    code cleanup by pylint

--- config.py
+++ config.py
@@ -5,114 +5,116 @@
 # email: zorrohunter@gmail.com
 # Created Time: 2009年05月31日 星期日 03时15分44秒
 # File Name: config.py
-# Description: 
+# Description:
 #########################################################################
-from md5sum import md5sum
 import sys
 import os
 import exceptions
 import bsddb
+from md5sum import md5sum
 from ConfigParser import ConfigParser
-home=os.environ['HOME']
-cfghome=os.path.join(home,'.pychmviewer')
-globalcfgfile=os.path.join(cfghome,'config.cfg')
-encoding=sys.getfilesystemencoding()
+
+home = os.environ['HOME']
+cfghome = os.path.join(home, '.pychmviewer')
+globalcfgfile = os.path.join(cfghome, 'config.cfg')
+encoding = sys.getfilesystemencoding()
+
 class GlobalConfig(object):
     def __getlastdir(self):
         return self.__lastdir.decode(encoding)
-    def __setlastdir(self,v):
-        assert isinstance(v,unicode)
-        self.__lastdir=v.encode(encoding)
-    lastdir=property(__getlastdir,__setlastdir)
+    def __setlastdir(self, v):
+        assert isinstance(v, unicode)
+        self.__lastdir = v.encode(encoding)
+    lastdir = property(__getlastdir, __setlastdir)
 
     def __init__(self):
-        cfg=ConfigParser()
+        cfg = ConfigParser()
         try:
             cfg.read(globalcfgfile)
         except:
             pass
         if not cfg.has_section('userconfig'):
-            self.loadlasttime=True
-            self.openremote=True
-            self.fontfamily=None
-            self.fontsize=None
-            self.__lastdir=home
-            self.sengine_own=True
+            self.loadlasttime = True
+            self.openremote = True
+            self.fontfamily = None
+            self.fontsize = None
+            self.__lastdir = home
+            self.sengine_own = True
         if not cfg.has_section('searchext'):
-            self.searchext={'html':True,'htm':True,'js':False,'txt':False,'css':False}
+            self.searchext = {'html':True, 'htm':True, 'js':False, 'txt':False, 'css':False}
         else:
-            self.searchext={}
+            self.searchext = {}
             for a,b in cfg.items('searchext'):
-                if b.lower()=='false':
-                    self.searchext[a]=False
+                if b.lower() == 'false':
+                    self.searchext[a] = False
                 else:
-                    self.searchext[a]=True
+                    self.searchext[a] = True
         try:
-            self.loadlasttime=cfg.getboolean('userconfig','loadlasttime')
+            self.loadlasttime = cfg.getboolean('userconfig', 'loadlasttime')
         except:
-            self.lastlasttime=True
+            self.lastlasttime = True
         try:
-            self.sengine_own=cfg.getboolean('userconfig','default_search_engine')
+            self.sengine_own = cfg.getboolean('userconfig', 'default_search_engine')
         except:
-            self.sengine_own=True
+            self.sengine_own = True
         try:
-            self.openremote=cfg.getboolean('userconfig','openremote')
+            self.openremote = cfg.getboolean('userconfig', 'openremote')
         except:
-            self.openremote=True
+            self.openremote = True
         try:
-            self.fontfamily=cfg.get('userconfig','fontfamily').decode('utf-8')
-            if self.fontfamily=='default':
-                self.fontfamily=None
+            self.fontfamily = cfg.get('userconfig','fontfamily').decode('utf-8')
+            if self.fontfamily == 'default':
+                self.fontfamily = None
         except:
-            self.fontfamily=None
+            self.fontfamily = None
         try:
-            self.fontsize=cfg.getint('userconfig','fontsize')
+            self.fontsize = cfg.getint('userconfig', 'fontsize')
         except:
-            self.fontsize=None
+            self.fontsize = None
         try:
-            self.__lastdir=cfg.get('userconfig','lastdir')
+            self.__lastdir = cfg.get('userconfig', 'lastdir')
         except:
-            self.__lastdir=home
+            self.__lastdir = home
 
     def savecfg(self):
         cfg=ConfigParser()
         cfg.add_section('userconfig')
-        cfg.set('userconfig','loadlasttime',str(self.loadlasttime))
-        cfg.set('userconfig','openremote',str(self.openremote))
+        cfg.set('userconfig', 'loadlasttime', str(self.loadlasttime))
+        cfg.set('userconfig', 'openremote', str(self.openremote))
         if self.fontfamily:
-            cfg.set('userconfig','fontfamily',self.fontfamily.encode('utf-8'))
+            cfg.set('userconfig', 'fontfamily', self.fontfamily.encode('utf-8'))
         if self.fontsize:
-            cfg.set('userconfig','fontsize',str(self.fontsize))
-        cfg.set('userconfig','lastdir',self.__lastdir)
-        cfg.set('userconfig','default_search_engine',self.sengine_own)
+            cfg.set('userconfig', 'fontsize', str(self.fontsize))
+        cfg.set('userconfig', 'lastdir', self.__lastdir)
+        cfg.set('userconfig', 'default_search_engine', self.sengine_own)
         cfg.add_section('searchext')
         for a,b in self.searchext.iteritems():
-            cfg.set('searchext',a,str(b))
+            cfg.set('searchext', a, str(b))
         try:
-            os.mkdir(cfghome,0700)
+            os.mkdir(cfghome, 0700)
         except exceptions.OSError:
             pass
-        f=open(globalcfgfile,'wb')
+        f=open(globalcfgfile, 'wb')
         cfg.write(f)
         f.flush()
         f.close()
-        
+
 
 class PyChmConfig(object):
-    def __init__(self,chmpath):
-        assert isinstance(chmpath,unicode)
-        self.md5=md5sum(chmpath)
-        self.cfghome=os.path.join(home,'.pychmviewer')
+    def __init__(self, chmpath):
+        assert isinstance(chmpath, unicode)
+        self.md5 = md5sum(chmpath)
+        self.cfghome = os.path.join(home, '.pychmviewer')
+        try:
+            os.mkdir(self.cfghome, 0700)
+        except exceptions.OSError:
+            pass
+        self.cfghome = os.path.join(self.cfghome, self.md5)
         try:
             os.mkdir(self.cfghome,0700)
         except exceptions.OSError:
             pass
-        self.cfghome=os.path.join(self.cfghome,self.md5)
-        try:
-            os.mkdir(self.cfghome,0700)
-        except exceptions.OSError:
-            pass
-        bookmarkpath=os.path.join(self.cfghome,'bookmark.db')
-        lastconfpath=os.path.join(self.cfghome,'last.db')
-        self.bookmarkdb=bsddb.hashopen(bookmarkpath)
-        self.lastconfdb=bsddb.hashopen(lastconfpath)
+        bookmarkpath = os.path.join(self.cfghome, 'bookmark.db')
+        lastconfpath = os.path.join(self.cfghome, 'last.db')
+        self.bookmarkdb = bsddb.hashopen(bookmarkpath)
+        self.lastconfdb = bsddb.hashopen(lastconfpath)
