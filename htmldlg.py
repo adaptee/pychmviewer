2e69b9309f8b5666ad2c644bf2155a6490dc220d
Adaptee <adaptee@gmail.com>
2010-12-08 05:55:22 +0800

    code cleanup by pylint

--- htmldlg.py
+++ htmldlg.py
@@ -5,13 +5,13 @@
 # email: zorrohunter@gmail.com
 # Created Time: 2009年06月02日 星期二 03时10分59秒
 # File Name: htmldlg.py
-# Description: 
+# Description:
 #########################################################################
 
-from Ui_htmlsourcedlg import Ui_Dialog
 from PyQt4 import QtCore, QtGui
+from Ui_htmlsourcedlg import Ui_Dialog
 
-class HtmlDlg(QtGui.QDialog,Ui_Dialog):
-    def __init__(self,parent=None):
-        QtGui.QDialog.__init__(self,parent)
+class HtmlDlg(QtGui.QDialog, Ui_Dialog):
+    def __init__(self, parent=None):
+        QtGui.QDialog.__init__(self, parent)
         self.setupUi(self)
