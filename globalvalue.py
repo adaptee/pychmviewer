9022288831c39a37dce595b06d19691fa5e46529
Adaptee <adaptee@gmail.com>
2010-12-08 04:33:58 +0800

    cleanup code style

--- globalvalue.py
+++ globalvalue.py
@@ -5,13 +5,15 @@
 # email: zorrohunter@gmail.com
 # Created Time: 2009年05月26日 星期二 03时01分09秒
 # File Name: global.py
-# Description: 
+# Description:
 #########################################################################
-chmFile=None
-chmpath=None
-mainWindow=None
-currentwebview=None
-tabs=None
-encoding=None
+
 from config import GlobalConfig
-globalcfg=GlobalConfig()
+globalcfg = GlobalConfig()
+
+chmFile        = None
+chmpath        = None
+encoding       = None
+mainWindow     = None
+currentwebview = None
+tabs           = None
