385e1cae8a215b4a9d65a472213d054c183d07ea
Adaptee <adaptee@gmail.com>
2010-12-08 05:43:08 +0800

    code cleanup by pylint

--- md5sum.py
+++ md5sum.py
@@ -5,24 +5,24 @@
 # email: zorrohunter@gmail.com
 # Created Time: 2009年05月31日 星期日 02时49分00秒
 # File Name: md5sum.py
-# Description: 
+# Description:
 #########################################################################
-import io  
-import sys  
-import hashlib  
-import string
+import io
+import sys
+import hashlib
 
 def md5sum(filename):
-    assert isinstance(filename,unicode)
-    filename=filename.encode(sys.getfilesystemencoding())
-    m=hashlib.md5()
-    file=io.FileIO(filename,'rb')
-    bytes=file.read(51200)
-    while bytes!='':
+    assert isinstance(filename, unicode)
+    filename = filename.encode(sys.getfilesystemencoding())
+    m = hashlib.md5()
+    file = io.FileIO(filename,'rb')
+    bytes = file.read(51200)
+    while bytes != '':
         m.update(bytes)
-        bytes=file.read(8096)
+        bytes = file.read(8096)
     file.close()
     return m.hexdigest()
 
-if __name__=='__main__':
-    print md5sum(sys.argv[1].decode(sys.getfilesystemencoding()))
+if __name__ == '__main__':
+    filename = sys.argv[1].decode(sys.getfilesystemencoding() )
+    print (md5sum(filename))
