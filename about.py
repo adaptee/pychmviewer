ff1b98102567f6f3d8b80b5735399b042cbf4c58
Adaptee <adaptee@gmail.com>
2010-12-08 11:31:51 +0800

    code cleanup by pylint

--- about.py
+++ about.py
@@ -5,11 +5,12 @@
 # email: zorrohunter@gmail.com
 # Created Time: 2009年06月02日 星期二 06时45分13秒
 # File Name: about.py
-# Description: 
+# Description:
 #########################################################################
+from PyQt4 import QtGui
 from Ui_about import Ui_Dialog
-from PyQt4 import QtCore, QtGui
-class aboutdlg(QtGui.QDialog,Ui_Dialog):
-    def __init__(self,parent=None):
-        QtGui.QDialog.__init__(self,parent)
+
+class aboutdlg(QtGui.QDialog, Ui_Dialog):
+    def __init__(self, parent=None):
+        QtGui.QDialog.__init__(self, parent)
         self.setupUi(self)
