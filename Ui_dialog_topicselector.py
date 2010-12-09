bc9bb59a367f0102b8a440487941de2a2fb32dcd
Adaptee <adaptee@gmail.com>
2010-12-10 03:00:13 +0800

    Revert "recompile *.ui files"
    This reverts commit 24f3699050a1403f5757788cce89be2470a726b7.

--- Ui_dialog_topicselector.py
+++ Ui_dialog_topicselector.py
@@ -1,43 +1,38 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/mnt/personal/code/pychmviewer/dialog_topicselector.ui'
+# Form implementation generated from reading ui file '/home/john/git/pychmviewer/dialog_topicselector.ui'
 #
-# Created: Fri Dec 10 02:38:10 2010
-#      by: PyQt4 UI code generator 4.8.1
+# Created: Sat May 30 04:10:07 2009
+#      by: PyQt4 UI code generator 4.4.4
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
 
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    _fromUtf8 = lambda s: s
-
 class Ui_DialogTopicSelector(object):
     def setupUi(self, DialogTopicSelector):
-        DialogTopicSelector.setObjectName(_fromUtf8("DialogTopicSelector"))
+        DialogTopicSelector.setObjectName("DialogTopicSelector")
         DialogTopicSelector.resize(218, 258)
         self.vboxlayout = QtGui.QVBoxLayout(DialogTopicSelector)
         self.vboxlayout.setMargin(9)
         self.vboxlayout.setSpacing(6)
-        self.vboxlayout.setObjectName(_fromUtf8("vboxlayout"))
+        self.vboxlayout.setObjectName("vboxlayout")
         self.label = QtGui.QLabel(DialogTopicSelector)
         self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.label.setObjectName(_fromUtf8("label"))
+        self.label.setObjectName("label")
         self.vboxlayout.addWidget(self.label)
         self.list = QtGui.QListWidget(DialogTopicSelector)
-        self.list.setObjectName(_fromUtf8("list"))
+        self.list.setObjectName("list")
         self.vboxlayout.addWidget(self.list)
         self.buttonBox = QtGui.QDialogButtonBox(DialogTopicSelector)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.NoButton|QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.buttonBox.setObjectName("buttonBox")
         self.vboxlayout.addWidget(self.buttonBox)
 
         self.retranslateUi(DialogTopicSelector)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), DialogTopicSelector.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), DialogTopicSelector.reject)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), DialogTopicSelector.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), DialogTopicSelector.reject)
         QtCore.QMetaObject.connectSlotsByName(DialogTopicSelector)
 
     def retranslateUi(self, DialogTopicSelector):
