6a4f254512acae326271bf64fd48600b294aabfb
Adaptee <adaptee@gmail.com>
2010-12-10 02:39:28 +0800

    add missing import

--- pychmtopics.py
+++ pychmtopics.py
@@ -8,7 +8,7 @@
 # Description:
 #########################################################################
 from PyQt4 import QtGui
-#from PyQt4.QtGui import QTreeWidgetItem
+from PyQt4.QtGui import QTreeWidgetItem
 
 import globalvalue
 from utils import remove_comment, getchmfile
