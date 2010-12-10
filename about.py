cf9278f19d75671166b5bbab6196f55044360e18
Adaptee <adaptee@gmail.com>
2010-12-10 08:51:36 +0800

    rename class

--- about.py
+++ about.py
@@ -10,7 +10,7 @@
 from PyQt4 import QtGui
 from Ui_about import Ui_Dialog
 
-class aboutdlg(QtGui.QDialog, Ui_Dialog):
+class AboutDialog(QtGui.QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         QtGui.QDialog.__init__(self, parent)
         self.setupUi(self)
