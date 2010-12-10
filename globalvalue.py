7ec19276b06a31bbc4a3a3d2b50f50099b6447fe
Adaptee <adaptee@gmail.com>
2010-12-11 04:26:36 +0800

    [important] remove globalvalue.chmpath, totally; path info is already contained with PyChmFile class

--- globalvalue.py
+++ globalvalue.py
@@ -13,7 +13,7 @@ from option import PyChmViewerConfig as GlobalConfig
 globalcfg = GlobalConfig()
 
 chmfile        = None
-chmpath        = u""
+#chmpath        = u""
 encoding       = "gb18030"
 mainWindow     = None
 currentwebview = None
