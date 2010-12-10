1d12b00d4e092b8936414a1ff77ad39839ad6c86
Adaptee <adaptee@gmail.com>
2010-12-11 06:40:33 +0800

    fix typo

--- session.py
+++ session.py
@@ -9,11 +9,12 @@ from option import PyChmViewerConfig
 _, system_encoding = locale.getdefaultlocale()
 
 
-def Session(object):
+class Session(object):
     def __init__(self):
         self.config_dir = os.path.join( os.environ["HOME"], ".pychmviewer")
         self.config_filename = os.path.join(self.config_dir, "pychmviewer.cfg")
-        self.config = PyChmviewerConfig(self.config_filename)
+        self.config = PyChmViewerConfig(self.config_filename)
+
         self.system_encoding = locale.getdefaultlocale()[1]
 
 
