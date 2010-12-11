e2f81bb649d337765d1075127dd27a66cbc3cc02
Adaptee <adaptee@gmail.com>
2010-12-11 15:51:15 +0800

    code layout adjustment

--- session.py
+++ session.py
@@ -3,6 +3,7 @@
 
 import os
 import locale
+import bsddb
 
 from option import PyChmViewerConfig
 
@@ -11,14 +12,20 @@ _, system_encoding = locale.getdefaultlocale()
 
 class Session(object):
     def __init__(self, config_path=None):
-        self.config_dir = os.path.join( os.environ["HOME"], ".pychmviewer")
+        self.config_dir      = os.path.join( os.environ["HOME"], ".pychmviewer")
+        self.config          = self._getConfig(config_path)
+        self.snapshot        = self._getSnapshot()
+        self.system_encoding = locale.getdefaultlocale()[1]
 
+    def _getConfig(self, config_path=None):
         config_path = config_path or \
                       os.path.join(self.config_dir, "pychmviewer.cfg" )
+        return PyChmViewerConfig(config_path)
 
-        self.config = PyChmViewerConfig(config_path)
+    def _getSnapshot(self):
+        path = os.path.join(  self.config_dir, "snapshot.db" )
+        return bsddb.hashopen(path)
 
-        self.system_encoding = locale.getdefaultlocale()[1]
 
 
 
