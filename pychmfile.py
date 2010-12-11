00b38fbf5e5f8831582efa81e5b13df036aac73f
Adaptee <adaptee@gmail.com>
2010-12-11 19:39:26 +0800

    add __del__() for PyChmFile

--- pychmfile.py
+++ pychmfile.py
@@ -58,6 +58,10 @@ class PyChmFile(object):
         if path:
             self.loadFile(path, force_encoding)
 
+    def __del__(self):
+        self._chm.CloseCHM()
+        self.bookmarkdb.close()
+
     def initialize(self):
         self._chm            = CHMFile()
         self._title          = u""
