cc46689847a2588c75b166814ef4c73de10c8a20
Adaptee <adaptee@gmail.com>
2010-12-13 13:27:59 +0800

    get rid of this messy module

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 :
 
+import re
 
 def remove_comment(text):
     " hello, world.#string ==> hello, world"
@@ -28,3 +29,22 @@ def CachedProperty(func):
 
     return property(get)
 
+
+def parseChmURL(url):
+    '''
+    url:unicode
+    return: tuple(bool,unicode,unicode)
+            first item tell if it's a url pointing to another .CHM
+            second item is chm file,
+            third item is page.
+    '''
+    assert isinstance(url, unicode)
+
+    # [scheme]  ms-its:[chmpath]::[pagepath]
+    pattern = re.compile(u'^ms-its:(.*)::(.*)$', re.I)
+    match = pattern.search(url)
+    if match:
+        newchmfile = match.group(1)
+        page = match.group(2)
+        return (True, newchmfile, page)
+    return (False, None, None)
