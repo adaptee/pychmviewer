e94cd21b8098f0596167a0712147bd740888974c
Adaptee <adaptee@gmail.com>
2010-12-18 18:50:52 +0800

    shorter the code for calculating md5sum

--- md5sum.py
+++ md5sum.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python
 # coding: utf8
-#########################################################################
-# Author: Xinyu.Xiang(John)
-# email: zorrohunter@gmail.com
-# Created Time: 2009年05月31日 星期日 02时49分00秒
-# File Name: md5sum.py
-# Description:
-#########################################################################
+
 import io
 import hashlib
 
-def md5sum(filename):
-    md5 = hashlib.md5()
-    fileobj = io.FileIO(filename,'rb')
-    binaries = fileobj.read(51200)
-    while binaries != '':
-        md5.update(binaries)
-        binaries = fileobj.read(8096)
-    fileobj.close()
-    return md5.hexdigest()
+def md5sum2(filename):
+    hasher = hashlib.md5()
 
+    with io.FileIO(filename, 'rb') as stream:
+        hasher.update( stream.readall() )
+
+    return hasher.hexdigest()
